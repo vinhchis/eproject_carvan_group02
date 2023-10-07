272a71b8b0f86c54f8ab357c4cc462da8ef07702
Vinh Chi <134524234+vinhchis@users.noreply.github.com>
2023-10-07 15:58:14 +0700

    udate signup and signin component

--- src/components/SignIn/SignIn.jsx
+++ src/components/SignIn/SignIn.jsx
@@ -5,70 +5,130 @@ import {
   MDBCheckbox,
   MDBCol,
   MDBRow,
-}
-  from 'mdb-react-ui-kit';
+} from "mdb-react-ui-kit";
 import { FaFacebookF } from "react-icons/fa6";
 import { FaGoogle } from "react-icons/fa6";
 import { FaTwitter } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa6";
-import locobrand from '../../assets/image/sign/signin.png'
-import "./signIn.css"
+import locobrand from "../../assets/image/sign/signin.png";
+import "./signIn.css";
+import React from "react";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 const SignIn = () => {
   return (
-  
+    <Formik
+      initialValues={{ email: "", password: "" }}
+      onSubmit={(values, { setSubmitting }) => {
+        alert("You logged in successfully");
+        setTimeout(() => {
+          console.log("Logging in", values);
+          setSubmitting(true);
+        }, 5000);
+      }}
+      validationSchema={Yup.object().shape({
+        email: Yup.string()
+          .email()
+          .required("Cannot be left blank")
+          .matches(/(?=.*\w+[@]\w+[.]\w{3}([.]\w{2})?)/, "Email invalid"),
+        password: Yup.string()
+          .required("Password has not been entered")
+          .min(6, "Password is too short - must be at least 6 characters")
+          .max(12, "Password is too long - must be at most 6 characters")
+          .matches(/(?=.*[0-9])/, "Password must contain at least one number"),
+      })}
+    >
+      {(props) => {
+        const {
+          values,
+          touched,
+          errors,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        } = props;
+        return (
+          <div className="validate-layout container-fluid">
+            <MDBRow>
+              <MDBCol col="4" md="6">
+                <div className="d-flex flex-row align-items-center justify-content-center">
+                  <p className="lead fw-normal mb-0 me-3">
+                    <b>SIGN IN FORM</b>
+                  </p>
+                </div>
 
-    <MDBContainer fluid className="p-3 h-custom">
-        <MDBRow>
+                <form onSubmit={handleSubmit}>
+                  <label htmlFor="email">Email</label>
+                  <MDBInput
+                    wrapperClass="mb-4"
+                    size="lg"
+                    name="email"
+                    type="text"
+                    placeholder="Enter your email"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={errors.email && touched.email && "error"}
+                  />
+                  {errors.email && touched.email && (
+                    <div className="input-feedback">{errors.email}</div>
+                  )}
+                  <label htmlFor="email">Password</label>
+                  <MDBInput
+                    wrapperClass="mb-4"
+                    size="lg"
+                    name="password"
+                    type="password"
+                    placeholder="Enter your password"
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={errors.password && touched.password && "error"}
+                  />
+                  {errors.password && touched.password && (
+                    <div className="input-feedback">{errors.password}</div>
+                  )}
+                  <div className="d-flex justify-content-between mb-4">
+                    <MDBCheckbox
+                      name="flexCheck"
+                      value=""
+                      id="flexCheckDefault"
+                      label="Remember me"
+                    />
+                    <a href="#">Forgot password?</a>
+                  </div>
+                  <button
+                    type="submit"
+                    disabled={isSubmitting}
+                    className="mb-0 px-5"
+                  >
+                    Login
+                  </button>
+                  <p className="small fw-bold mt-2 pt-1 mb-2">
+                    Don't have an account?{" "}
+                    <a href="/signup" className="link-danger">
+                      Register
+                    </a>
+                  </p>
+                </form>
+              </MDBCol>
 
-          <MDBCol col='4' md='6'>
-            <div className="d-flex flex-row align-items-center justify-content-center">
-
-              <p className="lead fw-normal mb-0 me-3">Sign in with:</p>
-
-              <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-                <FaFacebookF />
-              </MDBBtn>
-
-              <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-                <FaGoogle />
-              </MDBBtn>
-
-              <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-                <FaTwitter />
-              </MDBBtn>
-
-              <MDBBtn tag='a' color='none' className='m-1' style={{ color: '#1266f1' }}>
-                <FaGithub />
-              </MDBBtn>
-            </div>
-
-            <div className="divider d-flex align-items-center my-4">
-              <p className="text-center fw-bold mx-2 mb-0">Or:</p>
-            </div>
-
-            <MDBInput wrapperClass='mb-4' label='Email address' id='formControlLg' type='email' size="lg" />
-            <MDBInput wrapperClass='mb-4' label='Password' id='formControlLg' type='password' size="lg" />
-
-            <div className="d-flex justify-content-between mb-4">
-              <MDBCheckbox name='flexCheck' value='' id='flexCheckDefault' label='Remember me' />
-              <a href="!#">Forgot password?</a>
-            </div>
-
-            <div className='text-center text-md-start mt-4 pt-2'>
-              <MDBBtn className="mb-0 px-5" size='lg' href='/brands'>Login</MDBBtn>
-              <p className="small fw-bold mt-2 pt-1 mb-2">Don't have an account? <a href="/signup" className="link-danger">Register</a></p>
-            </div>
-
-          </MDBCol>
-
-          <MDBCol col='10' md='6'>
-            <img src={locobrand} width={500} class="img-fluid" alt="Sample image" />
-
-          </MDBCol>
-        </MDBRow>
-    </MDBContainer>
+              <MDBCol col="10" md="6">
+                <img
+                  src={locobrand}
+                  width={800}
+                  class="img-fluid"
+                  alt="Sample image"
+                />
+              </MDBCol>
+            </MDBRow>
+          </div>
+        );
+      }}
+    </Formik>
   );
-}
+};
 
 export default SignIn;
--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -11,108 +11,133 @@ import { FaGoogle } from "react-icons/fa6";
 import { FaTwitter } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa6";
 import locobrand from "../../assets/image/sign/signup.png";
+import { Formik } from "formik";
+import * as Yup from "yup";
 
 const SignUp = () => {
   return (
-    <MDBContainer className="p-3 h-custom">
-      <MDBRow>
-        <MDBCol col="4" md="6">
-          <img
-            src={locobrand}
-            width={500}
-            class="img-fluid"
-            alt="Sample image"
-          />
-        </MDBCol>
+    <Formik
+      initialValues={{ name: "", username: "", email: "", password: "" }}
+      onSubmit={(values, { setSubmitting }) => {
+        alert("You have successfully registered");
+        setTimeout(() => {
+          console.log("Signup", values);
+          setSubmitting(false);
+        }, 5000);
+      }}
+      validationSchema={Yup.object().shape({
+        name: Yup.string().required("Cannot be left blank"),
+        username: Yup.string().required("Cannot be left blank"),
+        email: Yup.string()
+          .email()
+          .required("Cannot be left blank")
+          .matches(/(?=.*\w+[@]\w+[.]\w{3}([.]\w{2})?)/, "Email invalid"),
+        password: Yup.string()
+          .required("Password has not been entered")
+          .min(6, "Password is too short - must be at least 6 characters")
+          .max(12, "Password is too long - must be at most 6 characters")
+          .matches(/(?=.*[0-9])/, "Password must contain at least one number"),
+      })}
+    >
+      {(props) => {
+        const {
+          values,
+          touched,
+          errors,
+          isSubmitting,
+          handleChange,
+          handleBlur,
+          handleSubmit,
+        } = props;
+        return (
+          <div className="validate-layout container-fluid">
+            <MDBRow>
+              <MDBCol col="4" md="6">
+                <img
+                  src={locobrand}
+                  width={1000}
+                  class="img-fluid"
+                  alt="Sample image"
+                />
+              </MDBCol>
+              <MDBCol col="10" md="6">
+                <div className="d-flex flex-row align-items-center justify-content-center">
+                  <p className="lead fw-normal mb-0 me-3">
+                    <b>SIGN UP FORM</b>
+                  </p>
+                </div>
+                <form onSubmit={handleSubmit}>
+                  <label htmlFor="name">Name</label>
+                  <MDBInput
+                    wrapperClass="mb-4"
+                    name="name"
+                    type="text"
+                    placeholder="Enter your name"
+                    value={values.name}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={errors.name && touched.name && "error"}
+                  />
+                  {errors.name && touched.name && (
+                    <div className="input-feedback">{errors.name}</div>
+                  )}
+                  <label htmlFor="username">Username</label>
+                  <MDBInput
+                    wrapperClass="mb-4"
+                    name="username"
+                    type="text"
+                    placeholder="Enter your username"
+                    value={values.username}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={errors.username && touched.username && "error"}
+                  />
+                  {errors.username && touched.username && (
+                    <div className="input-feedback">{errors.username}</div>
+                  )}
 
-        <MDBCol col="10" md="6">
-          <div className="d-flex flex-row align-items-center justify-content-center">
-            <p className="lead fw-normal mb-0 me-3">Sign up with:</p>
-
-            <MDBBtn
-              tag="a"
-              color="none"
-              className="m-1"
-              style={{ color: "#1266f1" }}
-            >
-              <FaFacebookF />
-            </MDBBtn>
-
-            <MDBBtn
-              tag="a"
-              color="none"
-              className="m-1"
-              style={{ color: "#1266f1" }}
-            >
-              <FaGoogle />
-            </MDBBtn>
-
-            <MDBBtn
-              tag="a"
-              color="none"
-              className="m-1"
-              style={{ color: "#1266f1" }}
-            >
-              <FaTwitter />
-            </MDBBtn>
-
-            <MDBBtn
-              tag="a"
-              color="none"
-              className="m-1"
-              style={{ color: "#1266f1" }}
-            >
-              <FaGithub />
-            </MDBBtn>
+                  <label htmlFor="email">Email</label>
+                  <MDBInput
+                    wrapperClass="mb-4"
+                    name="email"
+                    type="text"
+                    placeholder="Enter your email"
+                    value={values.email}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={errors.email && touched.email && "error"}
+                  />
+                  {errors.email && touched.email && (
+                    <div className="input-feedback">{errors.email}</div>
+                  )}
+                  <label htmlFor="email">Password</label>
+                  <MDBInput
+                    wrapperClass="mb-4"
+                    name="password"
+                    type="password"
+                    placeholder="Enter your password"
+                    value={values.password}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    className={errors.password && touched.password && "error"}
+                  />
+                  {errors.password && touched.password && (
+                    <div className="input-feedback">{errors.password}</div>
+                  )}
+                  <button
+                    type="submit"
+                    disabled={isSubmitting}
+                    className="mb-2 px-5"
+                  >
+                    SignUp
+                  </button>
+                </form>
+              </MDBCol>
+            </MDBRow>
           </div>
-
-          <div className="divider d-flex align-items-center my-4">
-            <p className="text-center fw-bold mx-2 mb-0">Or:</p>
-          </div>
-
-          <MDBInput
-            wrapperClass="mb-4"
-            placeholder="Name"
-            id="form1"
-            type="text"
-          />
-          <MDBInput
-            wrapperClass="mb-4"
-            placeholder="Username"
-            id="form1"
-            type="text"
-          />
-          <MDBInput
-            wrapperClass="mb-4"
-            placeholder="Email"
-            id="form1"
-            type="email"
-          />
-          <MDBInput
-            wrapperClass="mb-4"
-            placeholder="Password"
-            id="form1"
-            type="password"
-          />
-
-          <div className="d-flex justify-content-between mb-4">
-            <MDBCheckbox
-              name="flexCheck"
-              value=""
-              id="flexCheckDefault"
-              label="Remember me"
-            />
-            <a href="!#">Forgot password?</a>
-          </div>
-
-          <div className="text-center text-md-start mt-4 pt-2">
-            <MDBBtn className="mb-0 px-5" size="lg" href="/signin">
-              Sign up
-            </MDBBtn>
-          </div>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
+        );
+      }}
+    </Formik>
   );
 };
 
